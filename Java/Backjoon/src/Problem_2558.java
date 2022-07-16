6975dbc37733416b7221553cacbadbbfd0cf7a73
Woongbin06 <kob05314@gmail.com>
2022-07-16 23:46:54 +0900

    20220716 commit

---
+++ Java/Backjoon/src/Problem_2558.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Problem_2558 {
+    public static void main(String[] args) {
+        int x, y;
+
+        Scanner sc = new Scanner(System.in);
+        x = sc.nextInt();
+        y = sc.nextInt();
+
+        System.out.println(x + y);
+    }
+}
